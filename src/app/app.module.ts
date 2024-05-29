1a918845403a0781fdcc2451c3cb40b2f6a1f2a4
Girish <girish@post.com>
2024-05-29 12:33:37 -0700

    Routes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,21 @@ import { NgModule } from "@angular/core";
 import { AppComponent } from "./app.component";
 import { BrowserModule } from "@angular/platform-browser";
 import { StoreModule } from "./store/store.module";
+import { RouterModule } from "@angular/router";
+import { StoreComponent } from "./store/store.component";
+import { CartDetailComponent } from "./store/cartDetail.component";
+import { CheckoutComponent } from "./store/checkout.component";
 
 @NgModule({
     declarations: [AppComponent],
-    imports: [BrowserModule, StoreModule],
+    imports: [BrowserModule, StoreModule,
+        RouterModule.forRoot([
+            {path:"store",component: StoreComponent},
+            {path:"cart",component:CartDetailComponent},
+            {path:"checkout",component: CheckoutComponent},
+            {path:"**", redirectTo: "/store"}
+        ])
+    ],
     providers: [],
     bootstrap: [AppComponent]
 })
--- src/app/store/store.component.ts
+++ src/app/store/store.component.ts
@@ -2,6 +2,7 @@ import { Component } from "@angular/core";
 import { ProductRepository } from "../model/product.repository";
 import { Product } from "../model/product.model";
 import { Cart } from "../model/cart.model";
+import { Router } from "@angular/router";
 
 @Component({
     selector: "store",
@@ -13,7 +14,8 @@ export class StoreComponent {
     selectedPage = 1;
 
     constructor(private repository: ProductRepository,
-        private cart: Cart
+        private cart: Cart,
+        private router:Router
     ){}
     get products(): Product[] {
         let pageIndex = (this.selectedPage - 1) * this.productsPerPage
@@ -40,5 +42,6 @@ export class StoreComponent {
 
     addProductToCart(product:Product){
         this.cart.addLine(product);
+        this.router.navigateByUrl("/cart");
     }
 }
