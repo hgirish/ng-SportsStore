d35312eb3a389414ca54290ef6aa20e18c137ec1
Girish <girish@post.com>
2024-05-29 12:13:24 -0700

    Add to Cart

--- src/app/store/store.component.ts
+++ src/app/store/store.component.ts
@@ -1,6 +1,7 @@
 import { Component } from "@angular/core";
 import { ProductRepository } from "../model/product.repository";
 import { Product } from "../model/product.model";
+import { Cart } from "../model/cart.model";
 
 @Component({
     selector: "store",
@@ -11,7 +12,9 @@ export class StoreComponent {
     productsPerPage = 4;
     selectedPage = 1;
 
-    constructor(private repository: ProductRepository){}
+    constructor(private repository: ProductRepository,
+        private cart: Cart
+    ){}
     get products(): Product[] {
         let pageIndex = (this.selectedPage - 1) * this.productsPerPage
         return this.repository.getProducts(this.selectedCategory)
@@ -30,11 +33,12 @@ export class StoreComponent {
         this.productsPerPage = Number(newSize);
         this.changePage(1);
     }
-    // get pageNumbers(): number[]{
-    //     return Array(Math.ceil(this.repository.getProducts(this.selectedCategory).length / this.productsPerPage))
-    //     .fill(0).map((x,i) => i + 1);
-    // }
+  
     get pageCount():number{
         return Math.ceil(this.repository.getProducts(this.selectedCategory).length/this.productsPerPage);
+    }
+
+    addProductToCart(product:Product){
+        this.cart.addLine(product);
     }
 }
