1b5d0041b05b369510d6c49e3e5843926a898ef8
Girish <girish@post.com>
2024-05-28 21:45:50 -0700

    store module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,10 +1,11 @@
 import { NgModule } from "@angular/core";
 import { AppComponent } from "./app.component";
 import { BrowserModule } from "@angular/platform-browser";
+import { StoreModule } from "./store/store.module";
 
 @NgModule({
     declarations: [AppComponent],
-    imports: [BrowserModule],
+    imports: [BrowserModule, StoreModule],
     providers: [],
     bootstrap: [AppComponent]
 })
