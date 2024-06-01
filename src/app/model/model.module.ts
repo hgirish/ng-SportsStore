2c55bbfc47999bdabd3004a8ba19c724ed322be7
Girish <girish@post.com>
2024-05-31 19:39:13 -0700

    auth guard

--- src/app/model/model.module.ts
+++ src/app/model/model.module.ts
@@ -6,10 +6,12 @@ import { Order } from "./order.model";
 import { OrderRepository } from "./order.repository";
 import { provideHttpClient } from "@angular/common/http";
 import { RestDataSource } from "./rest.datasource";
+import { AuthService } from "./auth.service";
 
 @NgModule({
     providers:[ProductRepository, StaticDataSource, Cart, Order, OrderRepository, 
         provideHttpClient(),
-    {provide: StaticDataSource, useClass:RestDataSource}]
+    {provide: StaticDataSource, useClass:RestDataSource},
+RestDataSource, AuthService]
 })
 export class ModelModule{}
