d4b303283cb4267cd1e7b6c3eff1f6728acd8bb2
Girish <girish@post.com>
2024-06-01 20:25:40 -0700

    order table filter

--- src/app/admin/material.module.ts
+++ src/app/admin/material.module.ts
@@ -8,10 +8,11 @@ import { MatTableModule } from "@angular/material/table";
 import { MatPaginatorModule } from "@angular/material/paginator";
 import { MatFormFieldModule } from "@angular/material/form-field";
 import { MatInputModule } from "@angular/material/input";
+import { MatCheckboxModule } from "@angular/material/checkbox";
 
 const features: any[]=[MatToolbarModule, MatSidenavModule, MatIconModule, 
     MatDividerModule, MatButtonModule, MatTableModule, MatPaginatorModule,
-MatFormFieldModule, MatInputModule, ];
+MatFormFieldModule, MatInputModule, MatCheckboxModule, ];
 
 @NgModule({
     imports:[features],
