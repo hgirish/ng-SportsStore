6bc34a62397d1313941c95c515dca5ef43915577
hgirish <girish@post.com>
2024-06-01 22:02:11 -0700

    build and server

--- server.js
+++ server.js
@@ -22,9 +22,9 @@ app.use(bodyParser.json());
 app.use(auth);
 app.use("/api", router);
 app.use(history());
-app.use("/", express.static("./dist/SportsStore"));
+app.use("/", express.static("./dist/SportsStore/browser"));
 
-app.listen(80, () => console.log("HTTP Server running on port 80"));
+app.listen(8080, () => console.log("HTTP Server running on port 8080"));
 
 if (enableHttps) {
   https.createServer(ssloptions, app).listen(443, () => console.log("HTTPS Server running on port 443"));
